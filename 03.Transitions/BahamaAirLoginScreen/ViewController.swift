9b403b874e8812ab690cd04cb98f95870f8989cb
Alonso Zhang <code@zhangwu.space>
2017-05-15 15:52:01 +0800

    03.Transitions

--- 03.Transitions/BahamaAirLoginScreen/ViewController.swift
+++ 03.Transitions/BahamaAirLoginScreen/ViewController.swift
@@ -73,6 +73,8 @@ class ViewController: UIViewController {
         label.textColor = UIColor(red: 0.89, green: 0.38, blue: 0.0, alpha: 1.0)
         label.textAlignment = .center
         status.addSubview(label)
+        
+        statusPosition = status.center
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -94,43 +96,72 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        UIView.animate(withDuration: 0.5, animations: {
+        UIView.animate(withDuration: 0.5) {
             self.heading.center.x += self.view.bounds.width
-        })
+        }
         
-        UIView.animate(withDuration: 0.5, delay: 0.3, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.0, options: [], animations: {
-            self.username.center.x += self.view.bounds.width
-        }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 0.3, usingSpringWithDamping: 0.6,
+                       initialSpringVelocity: 0.0, options: [], animations: {
+                        self.username.center.x += self.view.bounds.width
+        },
+                       completion: nil
+        )
         
-        UIView.animate(withDuration: 0.5, delay: 0.4, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.0, options: [], animations: {
-            self.password.center.x += self.view.bounds.width
-        }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 0.4, usingSpringWithDamping: 0.6,
+                       initialSpringVelocity: 0.0, options: [],
+                       animations: {
+                        self.password.center.x += self.view.bounds.width
+        },
+                       completion: nil
+        )
         
-        UIView.animate(withDuration: 0.5, delay: 0.5, options: [], animations: {
-            self.cloud1.alpha = 1.0
-        }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: [],
+                       animations: {
+                        self.cloud1.alpha = 1.0
+        },
+                       completion: nil
+        )
         
-        UIView.animate(withDuration: 0.5, delay: 0.7, options: [], animations: {
-            self.cloud2.alpha = 1.0
-        }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 0.7, options: [],
+                       animations: {
+                        self.cloud2.alpha = 1.0
+        },
+                       completion: nil
+        )
         
-        UIView.animate(withDuration: 0.5, delay: 0.9, options: [], animations: {
-            self.cloud3.alpha = 1.0
-        }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 0.9, options: [],
+                       animations: {
+                        self.cloud3.alpha = 1.0
+        },
+                       completion: nil
+        )
         
-        UIView.animate(withDuration: 0.5, delay: 1.1, options: [], animations: {
-            self.cloud4.alpha = 1.0
-        }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 1.1, options: [],
+                       animations: {
+                        self.cloud4.alpha = 1.0
+        },
+                       completion: nil
+        )
         
-        UIView.animate(withDuration: 0.5, delay: 0.5,
-                       usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [], animations: {
+        UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 0.0, options: [],
+                       animations: {
                         self.loginButton.center.y -= 30.0
                         self.loginButton.alpha = 1.0
-        }, completion: nil)
+        },
+                       completion: nil
+        )
+        
+        
+        animateCloud(cloud1)
+        animateCloud(cloud2)
+        animateCloud(cloud3)
+        animateCloud(cloud4)
     }
     
     func showMessage(index: Int) {
         label.text = messages[index]
+        
         UIView.transition(with: status, duration: 0.33,
                           options: [.curveEaseOut, .transitionCurlDown],
                           animations: {
@@ -138,7 +169,51 @@ class ViewController: UIViewController {
         },
                           completion: {_ in
                             //transition completion
+                            delay(seconds: 2.0) {
+                                if index < self.messages.count-1 {
+                                    self.removeMessage(index: index)
+                                } else {
+                                    //reset form
+                                    self.resetForm()
+                                }
+                            }
         }
+        )
+    }
+    
+    func removeMessage(index: Int) {
+        
+        UIView.animate(withDuration: 0.33, delay: 0.0, options: [],
+                       animations: {
+                        self.status.center.x += self.view.frame.size.width
+        },
+                       completion: {_ in
+                        self.status.isHidden = true
+                        self.status.center = self.statusPosition
+                        
+                        self.showMessage(index: index+1)
+        }
+        )
+    }
+    
+    func resetForm() {
+        UIView.transition(with: status, duration: 0.2, options: .transitionFlipFromTop,
+                          animations: {
+                            self.status.isHidden = true
+                            self.status.center = self.statusPosition
+        },
+                          completion: nil
+        )
+        
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: [],
+                       animations: {
+                        self.spinner.center = CGPoint(x: -20.0, y: 16.0)
+                        self.spinner.alpha = 0.0
+                        self.loginButton.backgroundColor = UIColor(red: 0.63, green: 0.84, blue: 0.35, alpha: 1.0)
+                        self.loginButton.bounds.size.width -= 80.0
+                        self.loginButton.center.y -= 60.0
+        },
+                       completion: nil
         )
     }
     
@@ -147,20 +222,40 @@ class ViewController: UIViewController {
     @IBAction func login() {
         view.endEditing(true)
         
-        UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: {
+        UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping: 0.2,
+                       initialSpringVelocity: 0.0, options: [],
+                       animations: {
             self.loginButton.bounds.size.width += 80.0
-        }, completion: {_ in
+        },
+                       completion: {_ in
             self.showMessage(index: 0)
         }
         )
         
         UIView.animate(withDuration: 0.33, delay: 0.0, usingSpringWithDamping:
-            0.7, initialSpringVelocity: 0.0, options: [], animations: {
+            0.7,
+                       initialSpringVelocity: 0.0, options: [], animations: {
                 self.loginButton.center.y += 60.0
                 self.loginButton.backgroundColor = UIColor(red: 0.85, green: 0.83, blue: 0.45, alpha: 1.0)
                 self.spinner.center = CGPoint(x: 40.0,y: self.loginButton.frame.size.height/2)
                 self.spinner.alpha = 1.0
-        }, completion: nil)
+        },
+                 completion: nil
+        )
+    }
+    
+    func animateCloud(_ cloud: UIImageView) {
+        let cloudSpeed = 60.0 / view.frame.size.width
+        let duration = (view.frame.size.width - cloud.frame.origin.x) * cloudSpeed
+        UIView.animate(withDuration: TimeInterval(duration), delay: 0.0, options: .curveLinear,
+                       animations: {
+                        cloud.frame.origin.x = self.view.frame.size.width
+        },
+                       completion: {_ in
+                        cloud.frame.origin.x = -cloud.frame.size.width
+                        self.animateCloud(cloud)
+        }
+        )
     }
     
     // MARK: UITextFieldDelegate
